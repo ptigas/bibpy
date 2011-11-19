460ec922ecc730d499faa5c830dea588bb7f5d63
Panagiotis Tigkas <ptigas@gmail.com>
2011-11-19 22:54:30 +0000

    minor fixes

--- bib.py
+++ bib.py
@@ -21,193 +21,191 @@ THE SOFTWARE.
 """
 
 import fileinput
-import sys
 import re
 import json
 
 def clear_comments(data):
-	res = re.sub(r"(%.*\n)",'',data)
-	res = re.sub(r"(comment [^\n]*\n)",'',res)
-	return res
-
-def tokenize(data):
-    for item in token_re.finditer(data):
-        i = item.group(0)
-        yield re.sub(r"[\n|\s]*",'',i) # eat new line chars
-
-def ERROR(s) :
-	print "ERROR: %s" % s 
-	sys.exit(-1)
+    """Return the bibtex content without comments"""
+    res = re.sub(r"(%.*\n)", '', data)
+    res = re.sub(r"(comment [^\n]*\n)", '', res)
+    return res
 
 class Bibparser() :
-	def tokenize(self) :
-		white = re.compile(r"[\n|\s]+")
-		token_re = re.compile(r"([^\s\n\"#%'(){}@,=]+|@|\"|{|}|=|,)")
-		for item in token_re.finditer(self.data):
-			i = item.group(0)			
-			yield white.sub('',i)			
+    """Main class for Bibtex parsing"""
 
-	def __init__(self, data) :
-		self.data = data	
-		self.token = None
-		self.token_type = None
-		self._next_token = self.tokenize().next
-		self.db = {}
-		self.mode = None
-		self.records = {}
-		self.line = 1
+    def tokenize(self) :
+        """Returns a token iterator"""
+        white = re.compile(r"[\n|\s]+")
+        token_re = re.compile(r"([^\s\n\"#%'(){}@,=]+|@|\"|{|}|=|,)")
+        for item in token_re.finditer(self.data):
+            i = item.group(0)           
+            yield white.sub('', i)           
 
-	def parse(self) :
-		while True :
-			try :
-				self.next_token()				
-				while self.database() :
-					pass			
-			except StopIteration :
-				break
+    def __init__(self, data) :
+        self.data = data    
+        self.token = None
+        self.token_type = None
+        self._next_token = self.tokenize().next
+        self.hashtable = {}
+        self.mode = None
+        self.records = {}        
 
-	def next_token(self):
-		self.token = self._next_token()		
-	
-	def database(self) :
-		if self.token == '@' :
-			self.next_token()
-			self.entry()
-	
-	def entry(self) :		
-		if self.token == 'string' :
-			self.mode = 'string'
-			self.string()
-			self.mode = None
-		else :
-			self.mode = 'record'
-			self.record()
-			self.mode = None
+    def parse(self) :
+        """Parses self.data and stores the parsed bibtex to self.rec"""
+        while True :
+            try :
+                self.next_token()               
+                while self.database() :
+                    pass            
+            except StopIteration :
+                break
 
-	def string(self) :		
-		if self.token == "string" :
-			self.next_token()
-			if self.token == "{" :
-				self.next_token()
-				self.field()
-				if self.token == "}" :
-					pass
-				else :						
-					raise NameError("} missing")
-	
-	def field(self) :
-		name = self.name()
-		if self.token == '=' :
-			self.next_token()
-			value = self.value()			
-			if self.mode == 'string' :
-				self.db[name] = value
-			return (name, value)			
-	
-	def value(self) :
-		value = ""
-		if self.token == '"' :		
-			val = []
-			while True:
-				self.next_token()
-				if self.token == '"' :
-					break
-				else :
-					val.append(self.token)
-			value = ' '.join(val)
-			if self.token == '"' :			
-				self.next_token()
-			else :
-				raise NameError("\" missing")
-		elif self.token == '{' :
-			val = []
-			c = 0
-			while True:
-				self.next_token()
-				if self.token == '{' :
-					c +=1
-				if self.token == '}' :				
-					c -= 1
-				if c < 0 :
-					break
-				else :
-					val.append(self.token)
-			value = ' '.join(val)
-			if self.token == '}' :
-				self.next_token()
-			else :
-				raise NameError("} missing")
-		elif self.token.isdigit() :
-			value = self.token
-			self.next_token()
-		else :
-			if self.token in self.db :
-				value = self.db[ self.token ]
-			else :
-				value = self.token			
-			self.next_token()
+    def next_token(self):
+        """Returns next token"""
+        self.token = self._next_token()     
+    
+    def database(self) :
+        """Database"""
+        if self.token == '@' :
+            self.next_token()
+            self.entry()
+    
+    def entry(self) :  
+        """Entry"""     
+        if self.token == 'string' :
+            self.mode = 'string'
+            self.string()
+            self.mode = None
+        else :
+            self.mode = 'record'
+            self.record()
+            self.mode = None
 
-		return value
-	
-	def name(self) :
-		name = self.token		
-		self.next_token()
-		return name
+    def string(self) :   
+        """String"""   
+        if self.token == "string" :
+            self.next_token()
+            if self.token == "{" :
+                self.next_token()
+                self.field()
+                if self.token == "}" :
+                    pass
+                else :                      
+                    raise NameError("} missing")
+    
+    def field(self) :
+        """Field"""
+        name = self.name()
+        if self.token == '=' :
+            self.next_token()
+            value = self.value()            
+            if self.mode == 'string' :
+                self.hashtable[name] = value
+            return (name, value)            
+    
+    def value(self) :
+        """Value"""
+        value = ""
+        if self.token == '"' :      
+            val = []
+            while True:
+                self.next_token()
+                if self.token == '"' :
+                    break
+                else :
+                    val.append(self.token)
+            value = ' '.join(val)
+            if self.token == '"' :          
+                self.next_token()
+            else :
+                raise NameError("\" missing")
+        elif self.token == '{' :
+            val = []
+            brac_counter = 0
+            while True:
+                self.next_token()
+                if self.token == '{' :
+                    brac_counter += 1
+                if self.token == '}' :              
+                    brac_counter -= 1
+                if brac_counter < 0 :
+                    break
+                else :
+                    val.append(self.token)
+            value = ' '.join(val)
+            if self.token == '}' :
+                self.next_token()
+            else :
+                raise NameError("} missing")
+        elif self.token.isdigit() :
+            value = self.token
+            self.next_token()
+        else :
+            if self.token in self.hashtable :
+                value = self.hashtable[ self.token ]
+            else :
+                value = self.token          
+            self.next_token()
 
-	def key(self) :		
-		key = self.token
-		self.next_token()
-		return key
+        return value
+    
+    def name(self) :
+        """Returns parsed Name"""
+        name = self.token       
+        self.next_token()
+        return name
 
-	def record(self) :	
-		if self.token not in ['comment','string','preample'] :			
-			self.next_token()
-			if self.token == '{' :
-				self.next_token()
-				key = self.key()
-				self.records[ key ] = {}
-				if self.token == ',' :				
-					while True:
-						self.next_token()
-						field = self.field()
-						if field :
-							k = field[0]
-							v = field[1]
-							self.records[ key ][k] = v
-						if self.token <> ',' :						
-							break				
-					if self.token == '}' :
-						pass
-					else :
-						# assume entity ended
-						if self.token == '@' :
-							pass
-						else :
-							print self.token
-							raise NameError("@ missing")
-	
-	def json(self) :
-		return json.dumps(self.records)
+    def key(self) : 
+        """Returns parsed Key"""    
+        key = self.token
+        self.next_token()
+        return key
+
+    def record(self) : 
+        """Record""" 
+        if self.token not in ['comment', 'string', 'preample'] :          
+            self.next_token()
+            if self.token == '{' :
+                self.next_token()
+                key = self.key()
+                self.records[ key ] = {}
+                if self.token == ',' :              
+                    while True:
+                        self.next_token()
+                        field = self.field()
+                        if field :
+                            k = field[0]
+                            val = field[1]
+                            self.records[ key ][k] = val
+                        if self.token != ',' :                      
+                            break               
+                    if self.token == '}' :
+                        pass
+                    else :
+                        # assume entity ended
+                        if self.token == '@' :
+                            pass
+                        else :
+                            print self.token
+                            raise NameError("@ missing")
+    
+    def json(self) :
+        """Returns json formated records"""
+        return json.dumps(self.records)
 
 def main() :
-	data = ""
-	for line in fileinput.input():
-		line = line.rstrip()
-		#print line
-		data += line + "\n"
+    """Main function"""
+    data = ""
+    for line in fileinput.input():
+        line = line.rstrip()
+        #print line
+        data += line + "\n"
 
-	data = clear_comments(data)
-	
-	bib = Bibparser(data)
-	bib.parse()
-
-	print bib.json()
-	sys.exit(-1)
-	for key in parser.records :
-		print key	
-		for attr in parser.records[key] :		
-			if attr :
-				print "\t %s : %s" % (attr, parser.records[key][attr])
-
+    data = clear_comments(data)
+    
+    bib = Bibparser(data)
+    bib.parse()
+    print bib.json()
+    
 if __name__ == "__main__" :
-	main()
+    main()
