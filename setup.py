c366c4b95ddfda3f95f5a92279f9bb44c034f1e7
Panagiotis Tigas <ptigas@gmail.com>
2014-10-08 00:19:52 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Panagiotis Tigas
+
+from setuptools import setup, find_packages
+
+setup(name='bibpy',
+      version='1.0',
+      description='Bib parser',
+      author='Panagiotis Tigas',
+      author_email='ptigas@gmail.com',
+      packages=['bibpy', 'tests']
+     )
